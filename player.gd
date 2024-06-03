b0f0f19fe5fec98f28fd7afb4c0064f8c58f98b0
amunwes <56089330+amunwes@users.noreply.github.com>
2024-06-03 01:04:17 -0700

    changed player character to character2d from area2d
    saw some dumb shit i did initially, changed it and modified the code to make it more robust

--- player.gd
+++ player.gd
@@ -1,24 +1,43 @@
-extends Area2D
+extends CharacterBody2D
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
+@export var move_speed : float = 200
+var input_direction : Vector2 = Vector2.ZERO
+var screen_boundary : PackedVector2Array 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-#	pass
-	screen_size = get_viewport_rect().size
+	var screen_size = get_viewport_rect().size
+	var player_size = $CollisionShape2D.shape.get_rect().size
+	screen_boundary.append(Vector2.ZERO + player_size/2) 
+	screen_boundary.append(screen_size - player_size/2) 
 #	hide() # Replace with function body.
+	
 
+
+func _process(delta):
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * move_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, move_speed)
+
+	move_and_slide()
+	position = position.clamp(screen_boundary[0], screen_boundary[1])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-		
-	position += velocity * delta * speed
-#	position += velocity
-	position = position.clamp(Vector2.ZERO, screen_size)
-	
+#func _process(delta):
+#	var input_direction = Input.get_axis("move_left", "move_right")
+#	if (input_direction):
+#		velocity.x = input_direction * move_speed
+##	var velocity = Vector2.ZERO # The player's movement vector.
+##	if Input.is_action_pressed("move_right"):
+##		velocity.x += 1
+##	if Input.is_action_pressed("move_left"):
+##		velocity.x -= 1
+#
+#	position += velocity * delta * move_speed
+#	position = position.clamp(Vector2.ZERO, screen_size)
+
